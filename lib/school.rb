4f5ca714de0658c900c6d9777521faf56497b06b
Sara McCombs <thesaramccombs@gmail.com>
2019-09-02 00:46:11 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+class School
+  attr_accessor :name, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(student_name, grade)
+    if roster[grade] == nil 
+      roster[grade] = [student_name]
+    else
+      roster[grade] << student_name
+    end
+  end
+  
+  def grade(grade_number)
+    roster[grade_number]
+  end
+  
+  def sort 
+    roster.sort
+  end
+end
